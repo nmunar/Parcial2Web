5b308994d63973840d9be5e7ff047b9c6d9b7790
Nicolas Munar <n.munar@uniandes.edu.co>
2020-12-04 18:42:45 -0500

    terminado

--- parcialapp/src/components/App.js
+++ parcialapp/src/components/App.js
@@ -21,16 +21,12 @@ function App(props) {
     if (navigator.onLine) {
       let URL = "";
       if (props.lan === "es") {
-        console.log(2);
         URL =
           "https://gist.githubusercontent.com/jhonatan89/e379fadf8ed0f5381a2d8f8f3dea90c3/raw/e2bc20df02828d297f99558551e37959ac97a6f8/pokemon-es.json";
       } else {
-        console.log(1);
         URL =
           "https://gist.githubusercontent.com/jhonatan89/2089276d3ce0faceff8e55fc3459b818/raw/30ee1a77b3e328108faaaa9aaac6f2ddaa3d3711/pokemons-en.json";
       }
-      console.log(props.lan);
-
       fetch(URL)
         .then((res) => res.json())
         .then((data) => {
